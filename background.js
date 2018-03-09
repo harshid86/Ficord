06228bf196b702487b160f294810fb5e150a4d0f
Harshid Dattani <harshid@gmail.com>
2018-03-09 13:07:54 +0000

    Add Dev panel and popup

--- background.js
+++ background.js
@@ -1,1 +1,16 @@
-console.log("background test")
+console.log("background test");
+
+//recording = [];
+
+function handleMessage (message) {
+  console.log(message);
+  if (message.action === 'url') {
+    //this.lastUrl = message.value
+  } else {
+    //recording.push(message)
+  }
+}
+
+browser.runtime.onMessage.addListener(request => {
+  console.log("Message from the background script:");
+});
--- ficord.js
+++ ficord.js
@@ -1,1 +1,45 @@
-console.log("FICORD! FICUS! FIVIUM!")
+console.log("FICORD!");
+
+function inject() {
+  var clickable = document.querySelectorAll('a, button');
+  var typeable = document.querySelectorAll('input, textarea');
+  //var currentText = "";
+  
+  for (let i = 0; i < clickable.length; i++) {
+    clickable[i].addEventListener('click', handleClick);
+  }
+  
+  for (let i = 0; i < typeable.length; i++) {
+    typeable[i].addEventListener('keydown', handleKeydown);
+  }
+}
+
+function handleClick (e) {
+  console.log("click");
+  sendMessage(e)
+}
+
+function handleKeydown (e) {
+  console.log("type: " + e.target.value);
+  if (e.keyCode !== 9) {
+    //return
+  }
+  sendMessage(e)
+}
+
+function sendMessage (e) {
+  if (e.target.href) {
+    browser.runtime.sendMessage({
+      action: 'url',
+      value: e.target.href
+    })
+  } else {
+    browser.runtime.sendMessage({
+      //selector: selector.getSelector(e.target),
+      value: e.target.value,
+      action: e.type
+    })
+  }
+}
+
+inject();
